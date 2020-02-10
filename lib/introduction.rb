d03ebb1d1356fd46a4e63b43c0ed3404e185517d
inherited-snippet-0885 <toddmnorton@live.com>
2020-02-10 16:51:56 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,11 @@
-def introduction_with_language
+name = "Todd"
+language = "Ruby"
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
